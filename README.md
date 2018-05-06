d8eda79f9fcf6b5323f31402fa6e801c3b77a25f
Ivan <ivan.lin1993@gmail.com>
2018-05-06 12:17:08 +0800

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Steam Discount
+
+This is a simple program for the player who want to play cheap games.
+
+
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-page = 1
-DOLOR_SIGN = "NT$"
+config = open('config','r')
+
+DOLLOR_SIGN = config.read()
 
 
-def get_special_page(page):
+def get_special_page(filename,page):
   url = "http://store.steampowered.com/search/?specials=1&page=" + str(page)
   res = requests.get(url)
   soup= BeautifulSoup(res.text, 'html.parser')
@@ -14,9 +15,6 @@ def get_special_page(page):
   game_list=soup.find_all("a", { 'class':'search_result_row' })
   if len(game_list) == 0:
     return False
-  # search_result_row ds_collapse_flag app_impression_tracked
-  # col search_discount responsive_secondrow
-  # col search_price discounted responsive_secondrow
   for game in game_list:
     game_title = game.find("span",{"class":"title"}).text
     discount = game.find("div",{"class": "search_discount"}).text
@@ -26,31 +24,48 @@ def get_special_page(page):
     price = game.find("div",{"class": "search_price"}).text
     if "Free to Play" in price:
       price = price.split("Free to Play")
-      price = ["",price[0].replace(DOLOR_SIGN,""),"Free to Play"]
+      price = ["",price[0].replace(DOLLOR_SIGN,""),"Free to Play"]
     else:
-      price = price.split(DOLOR_SIGN)
+      price = price.split(DOLLOR_SIGN)
   
     try:
       price = price[2].replace(",","")
       if abs(int(discount)) > 70:
-        with open('games.csv', 'a', newline="", encoding='utf8') as csvfile:
-          spamwriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_NONE)
-          try:
-            spamwriter.writerow([game_title,discount+"%",DOLOR_SIGN + str(int(price)),game['href']])
-          except Exception as e:
-            print(e)
-            print(price)
-            spamwriter.writerow([game_title,"Free",DOLOR_SIGN + price,game['href']])
+        csvfile = open(filename, 'a', newline="", encoding='utf8')
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                          quotechar='"', quoting=csv.QUOTE_NONE)
+        try:
+          spamwriter.writerow([game_title,discount+"%",str(int(price)),game['href']])
+        except Exception as e:
+          print(e)
+          print(price)
+          spamwriter.writerow([game_title,"Free", price,game['href']])
         # print(game_title,discount+"%","NT$"+str(price))
     except Exception as e:
       print(e)
   return True
 
-while True:
-  if get_special_page(page) == False:
-    break
-  page+=1
-  print("Now {}".format(page), end = "\r")
-# get_special_page(1)
-print("\nDone")
+def init_csv(filename):
+  csvfile = open(filename, 'w', newline="", encoding='utf8')
+  spamwriter = csv.writer(csvfile, delimiter=',',
+                          quotechar='"', quoting=csv.QUOTE_NONE)
+  spamwriter.writerow(["Name","Discount","Price ({})".format(DOLLOR_SIGN),"Url"])
+
+
+def main():
+  page = 1
+  filename = "game.csv"
+  init_csv(filename)
+  while True:
+    if get_special_page(filename,page) == False:
+      break
+    page+=1
+    print("Now {}".format(page), end = "\r")
+  # get_special_page(1)
+  print("\nDone")
+
+if __name__ == '__main__':
+  main()
+
+
+
